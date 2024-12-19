12716be90c8666c63dc745c9bf3117c468c55aed
Muhammad Zain <muhammadzainrajput82@gmail.com>
2024-12-19 15:32:42 +0500

    changes

--- src/app/components/WordingCareer.tsx
+++ src/app/components/WordingCareer.tsx
@@ -1,14 +1,10 @@
 export default function WordingCareer() {
   return (
-    <div className="flex items-center justify-center p-4 mt-2">
+    <div className="flex items-center justify-center p-10 mt-10 bg-[#181815]">
       <div className="text-center max-w-3xl">
-        <p className="font-['Clash_Display'] font-light sm:text-3xl md:text-4xl leading-relaxed text-transparent bg-clip-text bg-gradient-to-r from-white to-[#999999]">
-          At{" "}
-          <span className=" font-medium bg-gradient-to-r from-[#D43833] to-[#650300] bg-clip-text text-transparent">
-            360XpertSolutions
-          </span>
-          , we believe in the power of balance. We give our best at work and
-          then fully embrace our time off
+        <p className="font-['Clash_Display'] font-light sm:text-3xl md:text-4xl leading-relaxed text-white">
+          At <span className="font-bold text-red-600">360Xpert Solutions,</span> we believe in the power
+          <span>of balance, We give our best at work and then fully</span> embrace our time off.
         </p>
       </div>
     </div>
